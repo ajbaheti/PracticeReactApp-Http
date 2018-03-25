c886e9121099c3e555a7544517a7a6a0992a3e94
Ashish Baheti <ajb9736@rit.edu>
2018-03-24 21:34:01 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+//axios.defaults.headers
+
+axios.interceptors.request.use(req => {
+    // console.log(req);
+    //edit request config
+    return req;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(res => {
+    // console.log(res);
+    //edit response
+    return res;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
+registerServiceWorker();
